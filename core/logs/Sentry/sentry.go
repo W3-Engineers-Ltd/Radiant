5121aef195be1ee2df1e09ee41c3be46e124ac73
partha <partha@w3engineers.com>
2022-02-08 18:53:24 +0600

    #Feature - Sentry added

---
+++ core/logs/Sentry/sentry.go
@@ -0,0 +1,190 @@
+package Sentry
+
+import (
+	"fmt"
+	"github.com/W3-Engineers-Ltd/Radiant/core/logs"
+	radiant "github.com/W3-Engineers-Ltd/Radiant/server/web"
+	"github.com/W3-Engineers-Ltd/Radiant/server/web/context"
+	"github.com/getsentry/sentry-go"
+	"html/template"
+	"runtime"
+	"time"
+)
+
+var tpl = `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>UpNext Application Error</title>
+    <style>
+        html, body, body * {padding: 0; margin: 0;}
+        #header {background:powderblue; border-bottom:solid 2px; padding: 20px 10px; text-align: center}
+        #header h2{ }
+        #footer {border-top:solid 1px #aaa; padding: 5px 10px; font-size: 12px; color:green;text-align: center}
+        #content {padding: 5px; min-height:300px}
+        #content .stack b{ font-size: 13px; color: red;}
+        #content .stack pre{padding-left: 10px;}
+        table {}
+        td.t {text-align: right; padding-right: 5px; color: #888;}
+    </style>
+    <script type="text/javascript">
+		var show = false;	
+		function showHide(){
+			var el = document.getElementById("error");
+			if(show===false){
+				el.style.display = "block";
+				show = true;
+			}else{
+				el.style.display = "none";
+				show = false;
+			}
+			
+		}
+    </script>
+</head>
+<body>
+    <div id="header">
+        <h2>UpNext Error</h2>
+    </div>
+    <div id="content">
+		<h3>Request Method: </td><td>{{.RequestMethod}}</h3>
+		<h3>Request URL: </td><td>{{.RequestURL}}</h3>
+		<h3>RemoteAddr: </td><td>{{.RemoteAddr}}</h3> 
+		<h3>UserAgent: </td><td>{{.UserAgent}}</h3> 
+	
+    </div>
+	<div id="error" style="display: none">
+		<pre>{{.AppError}}</pre>
+	</div>
+    <div id="footer">
+        <p onclick="showHide()">Powered by {{ .RadiantVersion }}</p>
+    </div>
+	
+</body>
+</html>
+`
+
+// ShowErr render default application error page with error and stack string.
+func showErr(err interface{}, ctx *context.Context, stack string) {
+	t, _ := template.New("upnexterrortemplate").Parse(tpl)
+
+	data := map[string]string{
+		"AppError":       fmt.Sprintf("%s:%v\n%s", ctx.Input.Host(), err, stack),
+		"RequestMethod":  ctx.Input.Method(),
+		"RequestURL":     ctx.Input.URI(),
+		"RemoteAddr":     ctx.Input.IP(),
+		"UserAgent":      ctx.Input.UserAgent(),
+		"RadiantVersion": "UpNext",
+	}
+	_ = t.Execute(ctx.ResponseWriter, data)
+}
+
+func SentryCaptureException(ctx *context.Context, err error) {
+	sentry.ConfigureScope(func(scope *sentry.Scope) {
+		scope.SetRequest(ctx.Request)
+		scope.SetRequestBody(ctx.Input.RequestBody)
+		scope.SetExtra("UserIP", ctx.Input.IP())
+	})
+
+	sentry.CaptureException(err)
+}
+
+func SentryCaptureMessage(ctx *context.Context, msg string) {
+	sentry.ConfigureScope(func(scope *sentry.Scope) {
+		scope.SetRequest(ctx.Request)
+		scope.SetRequestBody(ctx.Input.RequestBody)
+		scope.SetExtra("UserIP", ctx.Input.IP())
+	})
+
+	sentry.CaptureMessage(msg)
+}
+
+var ErrorTracker = func(ctx *context.Context, config *radiant.Config) {
+	if err := recover(); err != nil {
+		if err == radiant.ErrAbort {
+			return
+		}
+
+		var stack string
+		for i := 1; ; i++ {
+			_, file, line, ok := runtime.Caller(i)
+			if !ok {
+				break
+			}
+			logs.Critical(fmt.Sprintf("%s:%d", file, line))
+			stack = stack + fmt.Sprintln(fmt.Sprintf("%s:%d", file, line))
+		}
+		if ctx.Output.Status == 0 || ctx.Output.Status > 499 {
+			sentry.ConfigureScope(func(scope *sentry.Scope) {
+				scope.SetRequest(ctx.Request)
+				scope.SetRequestBody(ctx.Input.RequestBody)
+				scope.SetExtra("UserIP", ctx.Input.IP())
+			})
+
+			sentry.CurrentHub().Recover(fmt.Sprintf("%v\n%s", err, stack))
+			sentry.Flush(time.Second * 5)
+		}
+
+		if !radiant.BConfig.RecoverPanic {
+			panic(err)
+		}
+		if radiant.BConfig.EnableErrorsShow {
+			if _, ok := radiant.ErrorMaps[fmt.Sprint(err)]; ok {
+				radiant.Exception(uint64(ctx.Output.Status), ctx)
+				return
+			}
+		}
+
+		logs.Error(err)
+		if ctx.Output.Status != 0 {
+			ctx.ResponseWriter.WriteHeader(ctx.Output.Status)
+		} else {
+			ctx.ResponseWriter.WriteHeader(500)
+		}
+		if radiant.BConfig.RunMode == radiant.DEV {
+			showErr(err, ctx, stack)
+		} else {
+			showErr(err, ctx, "No more info!")
+		}
+
+	}
+}
+
+func SentryInit() {
+	sentryDNS, _ := radiant.AppConfig.String("SentryDSN")
+	releaseVersion, _ := radiant.AppConfig.String("ASSET_HASH")
+	serverName, _ := radiant.AppConfig.String("ServerName")
+	runMode, _ := radiant.AppConfig.String("RunMode")
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:         sentryDNS,
+		Environment: runMode,
+		Release:     releaseVersion,
+		ServerName:  serverName,
+		Debug:       false,
+	})
+	if err != nil {
+		logs.Error("Sentry initialization error: %s", err)
+	} else {
+		fmt.Println("Sentry running....")
+	}
+	defer sentry.Flush(2 * time.Second)
+}
+
+// How to use sentry
+
+//Raise default panic error
+/*v := []string{"hello world"}
+fmt.Println(v[111])
+*/
+
+//for error log
+//_, aErr := radiant.AppConfig.String("HelloWorld")
+// utils.SentryCaptureException(ctx, aErr)
+// or,
+//sentry.CaptureException(aErr)
+
+//for any message
+// utils.SentryCaptureMessage(ctx, aErr)
+// or,
+//sentry.CaptureMessage("Your message as Hello world.")
--- go.mod
+++ go.mod
@@ -38,35 +38,15 @@ require (
 )
 
 require (
-	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/coreos/go-semver v0.3.0 // indirect
-	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/couchbase/gomemcached v0.1.4 // indirect
 	github.com/couchbase/goutils v0.1.2 // indirect
-	github.com/cupcake/rdb v0.0.0-20161107195141-43ba34106c76 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/edsrzf/mmap-go v1.1.0 // indirect
-	github.com/go-kit/log v0.2.0 // indirect
-	github.com/go-logfmt/logfmt v0.5.1 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/getsentry/sentry-go v0.12.0
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.32.1 // indirect
-	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
-	github.com/siddontang/rdb v0.0.0-20150307021120-fc89ed2e418d // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
-	go.etcd.io/etcd/api/v3 v3.5.2 // indirect
-	go.etcd.io/etcd/client/pkg/v3 v3.5.2 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.7.0 // indirect
 	go.uber.org/zap v1.20.0 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
-	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220204002441-d6cc3cc0770e // indirect
 )
